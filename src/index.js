2c903831e9cc12316d3a44f36cc07af325c2488f
Serge <serge.bayet1975@gmail.com>
2019-11-26 11:23:06 +0100

    fix tabs problem in code

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { ifError } from "assert";
 const version = '1.0.0';
 
 export default class algorithms {
-	constructor(params) {
-		console.log('hello from algorithms');
-	}
+  constructor(params) {
+    console.log('hello from algorithms');
+  }
 	/*
 	Rosetta Code: Gray code
 
@@ -15,22 +15,22 @@ export default class algorithms {
 
 	It is also useful for generating inputs for Karnaugh maps in order from left to right or top to bottom.
 	*/
-	gray(enc, number) {
+  gray(enc, number) {
 
-		if (enc) {
+    if (enc) {
 
-			// number XOR SHR(number)
+      // number XOR SHR(number)
 
-			return number ^ (number >> 1);
-		}
-		else {
-			let ret = number;
-			while (number = number >> 1) {
-				ret = ret ^ number;
-			}
-			return ret;
-		}
-	}
+      return number ^ (number >> 1);
+    }
+    else {
+      let ret = number;
+      while (number = number >> 1) {
+        ret = ret ^ number;
+      }
+      return ret;
+    }
+  }
 
 	/* Rosetta Code: Greatest common divisor
 
@@ -38,17 +38,17 @@ export default class algorithms {
 	
 	*/
 
-	gcd(a, b) {
-		if (b > 0) {
-			console.log('Appel récursif gcd(' + b + ', ' + a + ' % ' + b + ')');
-			console.log('            => gcd(' + b + ', ' + a % b + ')');
-			return (this.gcd(b, a % b));
-		}
-		else {
-			console.log('Résultat final b = 0  =>  ' + a);
-			return a;
-		}
-	}
+  gcd(a, b) {
+    if (b > 0) {
+      console.log('Appel récursif gcd(' + b + ', ' + a + ' % ' + b + ')');
+      console.log('            => gcd(' + b + ', ' + a % b + ')');
+      return (this.gcd(b, a % b));
+    }
+    else {
+      console.log('Résultat final b = 0  =>  ' + a);
+      return a;
+    }
+  }
 
 	/*
 	Rosetta Code: Greatest subsequential sum
@@ -58,26 +58,26 @@ export default class algorithms {
 	An empty subsequence is considered to have the sum of 0 ; thus if all elements are negative, the result must be the empty sequence.
 	*/
 
-	maximumSubsequence(population) {
-		let ret = [];
-		let startIndex = 0;
-		let endIndex = 0;
-		let max = 0;
-		for (let i = 0; i < population.length; i++) {
-			let k = 0;
-			for (let j = i; j < population.length; j++) {
-				k += population[j];
-				if (k > max) {
-					max = k;
-					startIndex = i;
-					endIndex = j;
-				}
-			}
-		}
-		ret = population.slice(startIndex, endIndex + 1);
+  maximumSubsequence(population) {
+    let ret = [];
+    let startIndex = 0;
+    let endIndex = 0;
+    let max = 0;
+    for (let i = 0; i < population.length; i++) {
+      let k = 0;
+      for (let j = i; j < population.length; j++) {
+        k += population[j];
+        if (k > max) {
+          max = k;
+          startIndex = i;
+          endIndex = j;
+        }
+      }
+    }
+    ret = population.slice(startIndex, endIndex + 1);
 
-		return ret;
-	}
+    return ret;
+  }
 	/*
 	Rosetta Code: Hailstone sequence
 
@@ -97,39 +97,39 @@ export default class algorithms {
     Show the number less than 100,000 which has the longest hailstone sequence together with that sequence's length. (But don't show the actual sequence!)
 
 	*/
-	hailstoneSequence() {
-		let res = [];
-		let res27;
-		// Good luck!
+  hailstoneSequence() {
+    let res = [];
+    let res27;
+    // Good luck!
 
-		res27 = calculate(27);
+    res27 = calculate(27);
 
-		let number, max = 0;
-		for (let i = 1; i <= 100000; i++) {
-			let l = calculate(i);
-			if (l.length > max) {
-				max = l.length;
-				number = i;
-			}
-		}
+    let number, max = 0;
+    for (let i = 1; i <= 100000; i++) {
+      let l = calculate(i);
+      if (l.length > max) {
+        max = l.length;
+        number = i;
+      }
+    }
 
-		function calculate(n) {
-			let sequence = [];
-			//let index 
-			while (n > 1) {
-				sequence.push(n);
-				if (n % 2 === 0) {
-					n = n / 2;
-				}
-				else {
-					n = 3 * n + 1;
-				}
-			}
-			sequence.push(1)
-			return sequence;
-		}
-		return [[...res27.slice(0, 4).concat([8, 4, 2, 1])], [max, number]];
-	}
+    function calculate(n) {
+      let sequence = [];
+      //let index 
+      while (n > 1) {
+        sequence.push(n);
+        if (n % 2 === 0) {
+          n = n / 2;
+        }
+        else {
+          n = 3 * n + 1;
+        }
+      }
+      sequence.push(1)
+      return sequence;
+    }
+    return [[...res27.slice(0, 4).concat([8, 4, 2, 1])], [max, number]];
+  }
 
 	/*
 	Rosetta Code: Happy numbers
@@ -140,25 +140,25 @@ export default class algorithms {
 
 	Implement a function that returns true if the number is happy, or false if not.
 	*/
-	happy(number) {
-		let cycle = [];
+  happy(number) {
+    let cycle = [];
 
-		while (number !== 1 && !cycle[number]) {
-			cycle[m] = true;
-			let m = 0;
-			let str = number.toString();
-			for (let i = 0; i < str.length; i++) {
-				m += str.charAt(i) ** 2;
-			};
-			number = m;
+    while (number !== 1 && !cycle[number]) {
+      cycle[m] = true;
+      let m = 0;
+      let str = number.toString();
+      for (let i = 0; i < str.length; i++) {
+        m += str.charAt(i) ** 2;
+      };
+      number = m;
 
-			console.log(number);
-		}
-		if (cycle[number]) {
-			return false;
-		}
+      console.log(number);
+    }
+    if (cycle[number]) {
+      return false;
+    }
 
-		return true
-	}
+    return true
+  }
 }
 
