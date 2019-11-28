02531e2b4e698aa0fc5f14b2fcc33ae5472307e6
Serge <serge.bayet1975@gmail.com>
2019-11-28 11:25:39 +0100

    add number letter count

--- src/index.js
+++ src/index.js
@@ -1101,4 +1101,98 @@ export default class algorithms {
       .split('')
       .reduce((a, b) => a + parseInt(b), 0);
   }
+  /*
+  Project Euler: Problem 17: Number letter counts
+
+  If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+  If all the numbers from 1 to given limit inclusive were written out in words, how many letters would be used?
+
+  NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+  */
+
+  numberLetterCounts(limit) {
+    let numbers = [];
+    for (let i = 1; i <= limit; i++) {
+      numbers.push(this.numberToLetters(i));
+    }
+    numbers = numbers.join('');
+    numbers = numbers.replace(/\s/g, '');
+
+
+    // Good luck!
+    return numbers.length;
+  }
+  numberToLetters(n) {
+    let numbers = {
+      1: 'one',
+      2: 'two',
+      3: 'three',
+      4: 'four',
+      5: 'five',
+      6: 'six',
+      7: 'seven',
+      8: 'eight',
+      9: 'nine',
+      10: 'ten',
+      11: 'eleven',
+      12: 'twelve',
+      13: 'thirteen',
+      14: 'fourteen',
+      15: 'fifteen',
+      16: 'sixteen',
+      17: 'seventeen',
+      18: 'eighteen',
+      19: 'nineteen',
+      20: 'twenty',
+      30: 'thirty',
+      40: 'forty',
+      50: 'fifty',
+      60: 'sixty',
+      70: 'seventy',
+      80: 'eighty',
+      90: 'ninety',
+      100: 'hundred',
+      1000: 'thousand'
+    }
+    let result = [];
+    let nString = n.toString().split('');
+    let unity = nString.length;
+
+    for (let i = 0; i < nString.length; i++) {
+
+      switch (unity % 3) {
+        case 0:     // Hundreds
+          if (numbers[nString[i]] !== undefined) {
+            result.push(numbers[nString[i]]);
+            result.push('hundred');
+            result.push('and');
+          }
+
+          break;
+        case 2:     // Tens
+          if (numbers[nString[i] + nString[i + 1]] !== undefined) {
+            result.push(numbers[nString[i] + nString[i + 1]]);
+            i++;
+          }
+          else if (numbers[nString[i] + '0'] !== undefined) {
+            result.push(numbers[nString[i] + '0']);
+          }
+          break;
+        case 1:     // Ones
+
+          result.push(numbers[nString[i]]);
+          if (unity == 4) {
+            result.push('thousand');
+          }
+          break;
+      }
+      unity--;
+    }
+    //console.log(result);
+    result = result.join(' ');
+    if (result.endsWith(" and ")) result = result.substr(0, result.length - 5);
+    return result;
+  }
+
 }
