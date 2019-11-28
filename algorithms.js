e2dc8dadb1e667f908fb66d01c8f38bd5b19ccd6
Serge <serge.bayet1975@gmail.com>
2019-11-28 09:38:13 +0100

    add longest Collatz sequence

--- algorithms.js
+++ algorithms.js
@@ -1113,6 +1113,56 @@
           });
         }
       }
+      /*
+      Project Euler: Problem 14: Longest Collatz sequence
+       The following iterative sequence is defined for the set of positive integers:
+       n → n/2 (n is even)
+       n → 3n + 1 (n is odd)
+       Using the rule above and starting with 13, we generate the following sequence:
+       13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+       It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+       Which starting number, under the given limit, produces the longest chain?
+       NOTE: Once the chain starts the terms are allowed to go above one million. 
+      */
+
+    }, {
+      key: "longestCollatzSequence",
+      value: function longestCollatzSequence(limit) {
+        // Good luck!
+        var result = 1;
+        var counter = Math.floor(limit / 2);
+        var maxTerms = 0;
+
+        while (counter <= limit) {
+          var nTerms = termsCollatz(counter);
+
+          if (nTerms > maxTerms) {
+            maxTerms = nTerms;
+            result = counter;
+          }
+
+          counter++;
+        }
+
+        return result;
+
+        function termsCollatz(start) {
+          var c = start;
+          var nTerms = 0;
+
+          while (c !== 1) {
+            if (c % 2 == 0) {
+              c = c / 2;
+            } else {
+              c = 3 * c + 1;
+            }
+
+            nTerms++;
+          }
+
+          return nTerms;
+        }
+      }
     }]);
 
     return algorithms;
--- src/index.js
+++ src/index.js
@@ -1016,4 +1016,56 @@ export default class algorithms {
       return array.map(x => (char.repeat(length) + x).substr(-length));
     }
   }
+  /*
+  Project Euler: Problem 14: Longest Collatz sequence
+
+  The following iterative sequence is defined for the set of positive integers:
+
+  n → n/2 (n is even)
+
+  n → 3n + 1 (n is odd)
+
+  Using the rule above and starting with 13, we generate the following sequence:
+
+  13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+
+  It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+  Which starting number, under the given limit, produces the longest chain?
+
+  NOTE: Once the chain starts the terms are allowed to go above one million. 
+  */
+  longestCollatzSequence(limit) {
+    // Good luck!
+
+    let result = 1;
+    let counter = Math.floor(limit / 2);
+    let maxTerms = 0;
+    while (counter <= limit) {
+      let nTerms = termsCollatz(counter);
+      if (nTerms > maxTerms) {
+        maxTerms = nTerms;
+        result = counter;
+      }
+      counter++;
+    }
+    return result;
+    function termsCollatz(start) {
+
+      let c = start;
+      let nTerms = 0;
+      while (c !== 1) {
+        if (c % 2 == 0) {
+          c = c / 2;
+        }
+        else {
+          c = 3 * c + 1;
+        }
+        nTerms++;
+      }
+
+      return nTerms;
+    }
+
+  }
 }
