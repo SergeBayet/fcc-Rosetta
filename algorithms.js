eea1f48be1a9ecdd52d799b5266293c7e77627ca
Serge <serge.bayet1975@gmail.com>
2019-11-28 12:18:46 +0100

    add quibble

--- algorithms.js
+++ algorithms.js
@@ -1345,6 +1345,29 @@
         comb_(0, 0);
         return returnValue;
       }
+      /*
+      Rosetta Code: Comma quibblingPassed
+         Comma quibbling is a task originally set by Eric Lippert in his blog.
+         Write a function to generate a string output which is the concatenation of input words from a list/sequence where:
+         An input of no words produces the output string of just the two brace characters ("{}")
+        An input of just one word, e.g. ["ABC"], produces the output string of the word inside the two braces, e.g. "{ABC}"
+        An input of two words, e.g. ["ABC", "DEF"], produces the output string of the two words inside the two braces with the words separated by the string " and ", e.g. "{ABC and DEF}"
+        An input of three or more words, e.g. ["ABC", "DEF", "G", "H"], produces the output string of all but the last word separated by ", " with the last word separated by " and " and all within braces; e.g. "{ABC, DEF, G and H}"
+       */
+
+    }, {
+      key: "quibble",
+      value: function quibble(words) {
+        // Good luck!
+        var ret = '{' + words.join(',') + '}';
+        var pos = ret.lastIndexOf(',');
+
+        if (pos !== -1) {
+          ret = ret.substr(0, pos) + " and " + ret.substr(pos + 1);
+        }
+
+        return ret;
+      }
     }]);
 
     return algorithms;
