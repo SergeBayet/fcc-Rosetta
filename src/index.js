c096fd348ed94f758c3830d800d5f696a7eb69cb
Serge <serge.bayet1975@gmail.com>
2019-11-29 16:04:35 +0100

    add josephus prisoners

--- src/index.js
+++ src/index.js
@@ -1906,4 +1906,41 @@ repeat this rule until no fraction in the list produces an integer when multipli
     }
     return true;
   }
+  /*
+  Josephus problem is a math puzzle with a grim description: n prisoners are standing on a circle, sequentially numbered from 0 to n−1
+
+.
+
+An executioner walks along the circle, starting from prisoner 0
+, removing every k
+
+-th prisoner and killing him.
+
+As the process goes on, the circle becomes smaller and smaller, until only one prisoner remains, who is then freed.
+
+For example, if there are n=5
+prisoners and k=2
+
+, the order the prisoners are killed in (let's call it the "killing sequence") will be 1, 3, 0, and 4, and the survivor will be #2.
+
+Given any n,k>0
+
+, find out which prisoner will be the final survivor.
+
+In one such incident, there were 41 prisoners and every 3rd prisoner was being killed (k=3
+
+).
+
+Among them was a clever chap name Josephus who worked out the problem, stood at the surviving position, and lived on to tell the tale.
+
+Which number was he?
+  */
+  josephus(n, k, s) {
+    s = s | 1
+    for (var ps = [], i = n; i--;) ps[i] = i
+    for (var ks = [], i = --k; ps.length > s; i = (i + k) % ps.length) ks.push(ps.splice(i, 1))
+    return ps[0] + 1;
+
+
+  }
 }
