7105a41fe7aaeb40e1007e1990ca50963302a37e
Serge <serge.bayet1975@gmail.com>
2019-11-29 15:28:41 +0100

    add jortsort

--- algorithms.js
+++ algorithms.js
@@ -2141,6 +2141,22 @@
         transpositions = transpositions / 2;
         return (m / s.length + m / t.length + (m - transpositions) / m) / 3;
       }
+      /*
+      Rosetta Code: JortSort
+       jortSort is a sorting toolset that makes the user do the work and guarantees efficiency because you don't have to sort ever again. It was originally presented by Jenn "Moneydollars" Schiffer at the prestigious JSConf.
+       jortSort should be a function that takes a single array of comparable objects as its argument. It then sorts the array in ascending order and compares the sorted array to the originally provided array. If the arrays match (i.e. the original array was already sorted), the function returns true. If the arrays do not match (i.e. the original array was not sorted), the function returns false.
+      */
+
+    }, {
+      key: "jortsort",
+      value: function jortsort(array) {
+        // Good luck!
+        for (var i = 1; i < array.length; i++) {
+          if (array[i - 1] > array[i]) return false;
+        }
+
+        return true;
+      }
     }]);
 
     return algorithms;
--- src/index.js
+++ src/index.js
@@ -1891,4 +1891,19 @@ repeat this rule until no fraction in the list produces an integer when multipli
     return (m / s.length + m / t.length + (m - transpositions) / m) / 3;
 
   }
+
+  /*
+  Rosetta Code: JortSort
+
+  jortSort is a sorting toolset that makes the user do the work and guarantees efficiency because you don't have to sort ever again. It was originally presented by Jenn "Moneydollars" Schiffer at the prestigious JSConf.
+
+  jortSort should be a function that takes a single array of comparable objects as its argument. It then sorts the array in ascending order and compares the sorted array to the originally provided array. If the arrays match (i.e. the original array was already sorted), the function returns true. If the arrays do not match (i.e. the original array was not sorted), the function returns false.
+  */
+  jortsort(array) {
+    // Good luck!
+    for (let i = 1; i < array.length; i++) {
+      if (array[i - 1] > array[i]) return false;
+    }
+    return true;
+  }
 }
